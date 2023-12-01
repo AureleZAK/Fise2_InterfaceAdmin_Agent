c652130511dd0d7268184730919bb02ea91787ab
CHEKIR <cherifa.chekir@esprit.tn>
2023-12-01 15:58:45 +0100

    add test RAM

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -3,6 +3,7 @@ import threading
 from fastapi.testclient import TestClient
 from server import app
 from monitor import MonitorTask
+from domain.models import Ram
 
 
 class MonitorTaskFake(MonitorTask):
@@ -13,8 +14,9 @@ class MonitorTaskFake(MonitorTask):
     and make deterministic test (deterministic = repeatable and known values)
     """
     interval: int = 0
-    cpu_percent: list[float] = ["10", "12"]
+    cpu_percent: list[float] = [10, 12]
     num_cores: int = 3
+    ram_info: Ram = Ram(total=12345678, percent=57.8)
 
     def monitor(self):
         pass
@@ -37,7 +39,7 @@ def test_get_cpu_usage():
     app.state.monitortask = MonitorTaskFake()
     response = client.get("/metrics/v1/cpu/usage")
     assert response.status_code == 200
-    assert response.json() == [{"id": 0, "usage": "10"}, {"id": 1, "usage": "12"}]
+    assert response.json() == [{"id": 0, "usage": 10}, {"id": 1, "usage": 12}]
     # restore monitortask for next test
     app.state.monitortask = save_app
 
@@ -47,3 +49,11 @@ def test_get_cpu_core():
     # we can test types but not values because they will change at each test.
     assert response.status_code == 200
     assert isinstance(response.json()["number"], int)
+
+def test_get_ram():
+    save_app = app.state.monitortask
+    app.state.monitortask = MonitorTaskFake()
+    response = client.get("/metrics/v2/ram/usage")
+    assert response.status_code == 200
+    assert response.json() == {"total": 12345678, "percent": 57.8}
+    app.state.monitortask = save_app

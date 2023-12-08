6b94d620b8a82bd45bddbfba35e1a1d0923c3ed0
LEPRETTRE <clem.leprettre@gmail.com>
2023-12-08 14:41:15 +0000

    Revert "Merge branch 'ajout_hostname' into 'main'"
    This reverts merge request !9

--- src/domain/services/__init__.py
+++ src/domain/services/__init__.py
@@ -1,11 +1,9 @@
 from .cpuservice import CpuService
 from .ramservice import RamService
 from .ipservice import IpService
-from .hostnameservice import HostService
 
 __all__ = [
     "CpuService",
     "RamService",
     "IpService",
-    "HostService",
 ]

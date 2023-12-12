5128799d2a999de67552e1633dde1341de0a9d2e
CHEKIR <cherifa.chekir@esprit.tn>
2023-12-12 15:14:26 +0100

    change port back

--- mydashboard/dashboard/urls.py
+++ mydashboard/dashboard/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 from .views import my_view, pie_chart, dashboard, home
 
 urlpatterns = [
-    path('dashboard/', dashboard, name='dashboard'),
-    path('my-view/', my_view, name='my_view'),
     path('pie_chart/', pie_chart, name='pie_chart'),
     path('home/', home, name='home' ),
     # autres patterns d'URL ici

42001c7322d316cdbefa3dcd30102ed31272ff6f
ZAK <aurele.zak@free.fr>
2023-12-20 19:10:43 +0100

    Modification du code pour le lint fini pour main.py, server.py, coverage.py, le dossier tests, le dossier monitor

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,3 +1,13 @@
+"""
+Module defining the exported entities for testing purposes.
+
+This block imports the `MonitorTaskFake` class from the `test_api` module and
+defines the list `__all__` containing the names of entities that will be exported
+when using the `from <module> import *` syntax.
+
+Exported entities:
+    - MonitorTaskFake: A class used for mocking the real monitor in testing scenarios.
+"""
 from .test_api import  MonitorTaskFake
 
 __all__ = [

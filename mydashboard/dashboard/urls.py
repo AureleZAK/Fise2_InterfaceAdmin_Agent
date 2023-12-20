3d769f432051128f51901e29191ddbc793107ab1
Corentin <corentin.laval@etu.univ-st-etienne.fr>
2023-12-20 19:46:42 +0100

    conflits

--- mydashboard/dashboard/urls.py
+++ mydashboard/dashboard/urls.py
@@ -1,10 +1,13 @@
 # dashboard/urls.py
 
 from django.urls import path
-from .views import my_view, pie_chart, dashboard, home
+from .views import my_view, pie_chart, dashboard, home, get_api_data
+from django.urls import path
+
 
 urlpatterns = [
     path('pie_chart/', pie_chart, name='pie_chart'),
     path('home/', home, name='home' ),
+    path('get_api_data/', get_api_data, name='get_api_data'),
     # autres patterns d'URL ici
 ]
--- mydashboard/dashboard/views.py
+++ mydashboard/dashboard/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 # Create your views here.
 # dashboard/views.py
 from django.http import HttpResponse
@@ -7,7 +5,25 @@ from django.shortcuts import render
 from .models import ServerData
 import plotly.express as px
 import pandas as pd
-from server import count_log
+import requests
+
+
+def get_api_data(request):
+    api_url = "http://localhost:8080/metrics/v1/log/log"
+
+    # Effectuer une requête GET vers l'API
+    response = requests.get(api_url)
+
+    # Vérifier si la requête a réussi (code 200)
+    if response.status_code == 200:
+        # Convertir les données JSON en un dictionnaire Python
+        api_data = response.json()
+
+        # Passer les données à votre modèle de rendu (template)
+        return render(request, 'pie_chart.html', {'api_data': api_data})
+    else:
+        # Gérer les erreurs de requête
+        return HttpResponse(f"Erreur de requête: {response.status_code}")
 
 def dashboard(request):
     data_points = ServerData.objects.all()  # Récupérez les données depuis la base de données
@@ -27,11 +43,11 @@ def my_view(request):
 def pie_chart(request):
     # Fake values for the pie chart (replace with your own data)
 
-    result, good, error, pagetotest = count_log("../src/tests/filelog.txt")
+
 
     labels = [200, 404]
 
-    values = [good, error]
+    values = [2, 1]
 
     # Create a Pandas DataFrame
     data = {'labels': labels, 'values': values}

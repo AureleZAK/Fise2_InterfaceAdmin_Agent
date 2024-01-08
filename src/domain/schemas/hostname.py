c647cb774cef3dc48d9e84cbc4f7eafe440202f8
ZAK <aurele.zak@free.fr>
2024-01-08 11:53:28 +0100

    Modification du tests apres les modifications du lint

--- src/domain/schemas/hostname.py
+++ src/domain/schemas/hostname.py
@@ -1,14 +1,4 @@
-"""
-Ce fichier contient le schéma de données pour le nom d'hôte.
-"""
-
 from pydantic import BaseModel
 
 class GetHostnameResponseSchema(BaseModel):
-    """
-    Modèle de données Pydantic pour représenter la réponse du nom d'hôte.
-
-    Attributs :
-        hostname (str) : Le nom d'hôte au format chaîne de caractères.
-    """
-    hostname: str
+    hostname : str
